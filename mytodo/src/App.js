bbb1ba2dc1b8ce5eecb430cafd7dabaf32df6cbc
claire256 <clairenabwami3@gmail.com>
2022-09-12 21:25:05 +0300

    Add new files

--- mytodo/src/App.js
+++ mytodo/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css';
 import {Navbar, Container} from 'react-bootstrap';
-import Login from './Components/Login'
-import Signup from './Components/Sign-up';
+// import Login from './Components/Login'
+// import Signup from './Components/Sign-up';
 
 const App = () => {
   return (
@@ -12,8 +13,17 @@ const App = () => {
          <Navbar.Brand><h1>Todo</h1></Navbar.Brand>
        </Container>
      </Navbar>
-    <Signup />
-    <Login />
+     <Router>
+       <Routes>
+         <Route exact path='/'>Home</Route>
+         <Route  path='/'>Todos</Route>
+         <Route  path='/'>Account</Route>
+         <Route  path='/'>Signup</Route>
+         <Route  path='/'>Login</Route>
+         <Route  path='/'>Not Found</Route>
+       </Routes>
+     </Router>
+    
     </div>
   );
 }
