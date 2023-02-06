276af70fb87021faacf3c21d7db8e9cfe5010e55
claire256 <clairenabwami3@gmail.com>
2023-02-06 13:01:29 +0300

    Add changes to API folder

--- mytodo/src/Api/User.jsx
+++ mytodo/src/Api/User.jsx
@@ -1,6 +1,21 @@
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 
+export const AddUser = async (user)=>{
+    try{
+    const response = await axios.post('/signup', user)
+    return response.data.data
+    }
+    catch(err){  
+        if(err.response.status === 400) {  
+        return err.response.data.data
+        }
+        else{
+            return 'Something went wrong'
+        }
+    }
+}
+
 export const LoginUser = async (user)=>{
     try{
         const response = await axios.post('/login', user)
@@ -16,6 +31,7 @@ export const LoginUser = async (user)=>{
     }
     
 }
+
 export const GetUser = async()=>{
     const token = localStorage.getItem('access_token')
     const decoded = jwt_decode(token) 
@@ -36,5 +52,3 @@ export const GetUser = async()=>{
     }
     
 }
-
-
