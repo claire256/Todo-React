bf39123a3413ac5ff635db3d8025d4c2c9bf09a9
claire256 <clairenabwami3@gmail.com>
2023-02-09 13:06:45 +0300

    Add chamges

--- mytodo/src/Pages/Account.jsx
+++ mytodo/src/Pages/Account.jsx
@@ -9,12 +9,11 @@ const Account = ()=>{
     useEffect(()=>{
      const fetchUser = async()=>{
        const fetchedUser = await GetUser(user)
-       console.log('fff',fetchedUser.data)
        setUser(fetchedUser.data)
      }
      fetchUser()
     }, [])
-    // console.log('user', user)
+
     return(
       <>
       <div style={{backgroundColor: 'rgb(211, 228, 228)', height: '870px'}}>
@@ -24,66 +23,11 @@ const Account = ()=>{
         <i className="fa-solid fa-circle-user"></i>
         <p style={{fontSize: '25px'}} className="pt-2">{user.first_name}</p>
         <p style={{fontSize: '25px'}} className="pt-2">{user.last_name}</p>
-        <p style={{fontSize: '25px'}} className="pt-2">{user.email}</p>
-        {/* <i class="fa-regular fa-pen-to-square"></i> */}
+        <p style={{fontSize: '25px'}} className="pt-2">{user.email}</p>       
         </div>
-        {/* <hr
-           style={{
-            background: 'lime',
-            color: 'red',
-            height: '3px',
-            width: '100%'
-          }}
-         /> */}
+        
         <div className="mt-5">
-          {/* <div className="acc-icons">
-        <i class="fa-regular fa-calendar-days"></i>
-        <p style={{fontSize: '19px'}}>Calendar</p>
-        </div> */}
-        {/* <hr
-           style={{
-            background: 'lime',
-            color: 'red',
-            height: '3px',
-            width: '100%'
-          }}
-         /> */}
-        {/* <div className="acc-icons"> */}
-        {/* <i class="fa-regular fa-envelope"></i> */}
-        {/* <p style={{fontSize: '19px'}}>Email Address</p> */}
-        {/* </div> */}
-        {/* <hr
-           style={{
-            background: 'lime',
-            color: 'red',
-            height: '3px',
-            width: '100%'
-          }}
-         /> */}
-        {/* <div className="acc-icons">
-        <i class="fa-solid fa-phone"></i>
-        <p style={{fontSize: '19px'}}>+256 788536892</p>
-        </div>
-        <hr
-           style={{
-            background: 'lime',
-            color: 'red',
-            height: '3px',
-            width: '100%'
-          }}
-         /> */}
-        {/* <div className="acc-icons">
-        <i class="fa-solid fa-gear"></i>
-        <p style={{fontSize: '19px'}}>Settings</p>
-        </div>
-        <hr
-           style={{
-            background: 'lime',
-            color: 'red',
-            height: '3px',
-            width: '100%'
-          }}
-         /> */}
+          
         <div className="acc-button">
         <Button style={{width: '200px'}} variant="success">Edit profile</Button>
         </div>
